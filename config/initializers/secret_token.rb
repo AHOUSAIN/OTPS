f869b7faf9443c3371d44583178e82227fa5dd32
AHOUSAIN <afrahhassancmu@gmail.com>
2012-11-22 12:16:48 +0300

    Created master app and Gemfile finished

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+OTPS::Application.config.secret_token = '9398f39d85e482f3d212bd086d39d5fb522954da86987db1edcc9b55118271bdfd461238d888d39f91368517ba9a6e79f69971d5be9b939b04103ffcdad86573'
